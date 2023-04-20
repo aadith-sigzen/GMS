b960307c91d2959f7953805c0a133e586096ae69
aadith-sigzen <aadith.p@sigzen.com>
2023-04-20 18:05:05 +0530

    feat:update the gym trainer doctype

--- gms/gms/doctype/gym_trainer/gym_trainer.js
+++ gms/gms/doctype/gym_trainer/gym_trainer.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Aadith and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Gym Member', {
+frappe.ui.form.on('Gym Trainer', {
 	// refresh: function(frm) {
 
 	// }
--- gms/gms/doctype/gym_trainer/gym_trainer.py
+++ gms/gms/doctype/gym_trainer/gym_trainer.py
@@ -1,17 +1,17 @@
 # Copyright (c) 2023, Aadith and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
-class GymMember(Document):
+class GymTrainer(Document):
 	def get_feed(self):
 		return self.full_name
 	def autoname(self):
-		full_name = self.first_name + ' ' + self.last_name
+		full_name = self.salutation +' '+ self.first_name + ' ' + self.last_name
 		self.full_name = full_name
 		self.name = full_name
 	def validate(self):
-		full_name = self.first_name + ' ' + self.last_name
+		full_name = self.salutation +' '+ self.first_name + ' ' + self.last_name
 		self.full_name = full_name
-
