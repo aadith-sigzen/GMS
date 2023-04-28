6774aa9e4d8f3d5657761045261bccc3dcf8497e
yash-sigzen <yash.b@sigzen.com>
2023-04-28 10:32:32 +0530

    gym

--- gms/gms/doctype/gym_membership/gym_membership.py
+++ gms/gms/doctype/gym_membership/gym_membership.py
@@ -11,6 +11,7 @@ from frappe.utils import today
 
 
 class GymMembership(Document):
+<<<<<<< Updated upstream
     def validate(self):
         if frappe.db.exists('Gym Membership', {"gym_member": self.gym_member}) and self.is_new():
             gym_membership = frappe.get_last_doc('Gym Membership', filters={"gym_member": self.gym_member})
@@ -28,5 +29,26 @@ class GymMembership(Document):
         else:
             self.total_amount = self.rate*12
             self.to_date= frappe.utils.add_days(self.from_date, 365)
+=======
+	def validate(self):
+		if frappe.db.exists('Gym Membership', {"gym_member": self.gym_member}) and self.is_new():
+			gym_membership = frappe.get_last_doc('Gym Membership', filters={"gym_member": self.gym_member})
+			if str(gym_membership.to_date) >= nowdate():
+				frappe.throw("You get alredy membership")
+		else:
+				frappe.msgprint(('Your Gym Workout Plan '))
+
+	def before_save(self):
+		if self.durnation == "Monthly":
+			self.total_amount = self.rate*1
+			self.to_date= frappe.utils.add_days(self.from_date, 30)
+		elif self.durnation == "Quarterly":
+			self.total_amount = self.rate*6
+			self.to_date= frappe.utils.add_days(self.from_date, 180)
+
+		else:
+			self.total_amount = self.rate*12
+			self.to_date= frappe.utils.add_days(self.from_date, 365)
+>>>>>>> Stashed changes
 
 
