530d1e38c404d691087989e16fee2e478ddcc091
dixit-sigzen <dixit.f@sigzen.com>
2023-04-29 15:23:09 +0530

    aa

---
+++ gms/gms/report/steps_progress_report/steps_progress_report.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2023, Aadith and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Steps Progress Report"] = {
+	"filters": [{
+		"fieldname":"gym_member",
+		"label":"Gym Member",
+		"fieldtype":"Link",
+		"options":"Gym Member"
+	},
+	{
+		"fieldname":"from_date",
+		"label":"From Date",
+		"fieldtype": "Date",
+
+	},
+	{
+		"fieldname":"to_date",
+		"label":"To Date",
+		"fieldtype": "Date",
+
+	}
+]
+};
