57314262a3fab20bc250d11b689d3d52cb54a2b4
aadith-sigzen <aadith.p@sigzen.com>
2023-04-29 12:05:45 +0530

    feat:update the gym membership list

--- gms/gms/doctype/gym_membership/gym_membership.js
+++ gms/gms/doctype/gym_membership/gym_membership.js
@@ -4,36 +4,38 @@
 frappe.ui.form.on('Gym Membership', {
 	durnation: function(frm) {
 		if(frm.doc.durnation=="1-Month plan"){
-			console.log(frm.doc.rate*1);
+			frm.set_value("to_date",frappe.datetime.add_months(frm.doc.from_date,1))
 			frm.set_value("total_amount",frm.doc.rate*1)}
 		else if(frm.doc.durnation=="3-Month plan") {
-			console.log(frm.doc.rate*1);
+			frm.set_value("to_date",frappe.datetime.add_months(frm.doc.from_date,3))
 			frm.set_value("total_amount",frm.doc.rate*3)
 		}
 		else if (frm.doc.durnation=="6-Month plan") {
-			console.log(frm.doc.rate*6);
+			frm.set_value("to_date",frappe.datetime.add_months(frm.doc.from_date,3))
 			frm.set_value("total_amount",frm.doc.rate*6)
 		}
 		else if (frm.doc.durnation=="12-Month plan") {
-			console.log(frm.doc.rate*12);
+			frm.set_value("to_date",frappe.datetime.add_months(frm.doc.from_date,12))
 			frm.set_value("total_amount",frm.doc.rate*12)
+		}else{
+			frm.set_value("to_date",null)
+			frm.set_value("total_amount",0)
 		}
 	},
 	gym_workout_plan: function(frm) {
 		if(frm.doc.durnation=="1-Month plan"){
-			console.log(frm.doc.rate*1);
 			frm.set_value("total_amount",frm.doc.rate*1)}
 		else if(frm.doc.durnation=="3-Month plan") {
-			console.log(frm.doc.rate*1);
 			frm.set_value("total_amount",frm.doc.rate*3)
 		}
 		else if (frm.doc.durnation=="6-Month plan") {
-			console.log(frm.doc.rate*6);
 			frm.set_value("total_amount",frm.doc.rate*6)
 		}
 		else if (frm.doc.durnation=="12-Month plan") {
-			console.log(frm.doc.rate*12);
 			frm.set_value("total_amount",frm.doc.rate*12)
+		}
+		else{
+			frm.set_value("total_amount",0)
 		}
 	}
 });
