934b22d57e27b5eb4996ece6872b8ff11ea77a6d
aadith-sigzen <aadith.p@sigzen.com>
2023-04-30 19:10:23 +0530

    feat:update the new test case

--- gms/gms/doctype/exercise_type/test_exercise_type.py
+++ gms/gms/doctype/exercise_type/test_exercise_type.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2023, Aadith and Contributors
 # See license.txt
 
-# import frappe
+import frappe
+import unittest
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestExerciseType(FrappeTestCase):
-	pass
+class TestExerciseType(unittest.TestCase):
+    def test_case1(self):
+        frappe.set_user("Administrator")
+        doc = frappe.get_doc({
+             "doctype": "Exercise Type",
+             "exercise_type" :"TEST2"
+             }).insert()
+
+
+
+
